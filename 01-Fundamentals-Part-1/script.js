30f1cd14facd2a2bc3f3f44fbb2c9e5251aa52e5
Assefa_Altaye <assekopsynwa@gmail.com>
2023-12-22 02:13:48 -0600

    Value Variable Data Tyep

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,38 @@
+/*
 let js = 'amazing';
-if (js === 'amazing') alert('Javascript is FUN!');
 console.log(40 + 8 + 23 - 10);
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Assefa';
+console.log(firstName);
+
+//Assignemnt
+let country = 'United States';
+let continent = 'North America';
+let population = '350,000';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Assefa');
+
+javaScriptIsFun = 'YES!';
+
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2023;
+console.log(typeof year);
+console.log(typeof null);
